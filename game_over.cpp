f71b24c27e38ab6ebb1e13ab0831f5f40c0501c7
Kyle Hassett <hassettkyle@gmail.com>
2017-03-14 13:20:33 -0400

    setting up the repository to show im learning c++

---
+++ game_over.cpp
@@ -0,0 +1,10 @@
+//Game Over Program, twist on the "Hello World" classic first step
+//First C++ program
+
+# include<iostream>
+using namespace std;
+
+int main(){
+   cout << "Game Over!" << endl;
+   return 0;
+}
